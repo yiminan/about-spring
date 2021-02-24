beec99e1884f87700b7d0f9be4704621cdb9d176
orgojy <giveawesome@gmail.com>
2021-02-24 16:57:03 +0900

    init commit

---
+++ src/main/java/com/example/aboutspring/AboutSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.aboutspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AboutSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aboutspring/AboutSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aboutspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
