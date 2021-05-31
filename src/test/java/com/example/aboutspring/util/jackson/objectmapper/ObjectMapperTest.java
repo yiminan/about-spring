7264af5106f27915616e9725753c302e25fb08ae
orgojy <giveawesome@gmail.com>
2021-05-31 14:15:06 +0900

    Add test method "writeValueAsString" for ObjectMapper to show parsed JSON

--- src/test/java/com/example/aboutspring/util/jackson/objectmapper/ObjectMapperTest.java
+++ src/test/java/com/example/aboutspring/util/jackson/objectmapper/ObjectMapperTest.java
@@ -2,16 +2,18 @@ package com.example.aboutspring.util.jackson.objectmapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ObjectMapperTest {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     private static class User {
 
@@ -37,14 +39,26 @@ class ObjectMapperTest {
         // given
         String pathname = "user.json";
         File resultFile = new File(pathname);
+        User user = new User("Ryan", 30);
 
         // when
-        ObjectMapper objectMapper = new ObjectMapper();
-        User user = new User("Ryan", 30);
         objectMapper.writeValue(resultFile, user);
 
         // then
         assertThat(Files.readAllLines(Paths.get(pathname)).get(0))
+                .isEqualTo("{\"name\":\"Ryan\",\"age\":30}");
+    }
+
+    @Test
+    void writeValueAsString() throws JsonProcessingException {
+        // given
+        User user = new User("Ryan", 30);
+
+        // when
+        String userAsString = objectMapper.writeValueAsString(user);
+
+        // then
+        assertThat(userAsString)
                 .isEqualTo("{\"name\":\"Ryan\",\"age\":30}");
     }
 }
