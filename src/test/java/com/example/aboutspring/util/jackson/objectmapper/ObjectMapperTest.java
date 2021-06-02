821e928e772669a258706e3d26e0dd4c2e51dfdd
orgojy <giveawesome@gmail.com>
2021-06-02 19:55:12 +0900

    Add test case to read value from String arrays to array list

--- src/test/java/com/example/aboutspring/util/jackson/objectmapper/ObjectMapperTest.java
+++ src/test/java/com/example/aboutspring/util/jackson/objectmapper/ObjectMapperTest.java
@@ -1,16 +1,19 @@
 package com.example.aboutspring.util.jackson.objectmapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import org.junit.jupiter.api.DisplayName;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class ObjectMapperTest {
@@ -80,9 +83,13 @@ class ObjectMapperTest {
                 .isEqualTo(30);
     }
 
-    @DisplayName("test")
     @Test
-    void test() {
-
+    void readValue() throws JsonProcessingException {
+        String jsonArr = "[{\"name\":\"Ryan\",\"age\":30},{\"name\":\"Jake\",\"age\":20}]";
+        List<User> users = objectMapper.readValue(jsonArr, new TypeReference<>() {
+        });
+        assertThat(users).hasSize(2);
+        assertThat(users).extracting("name", "age")
+                .contains(tuple("Ryan", 30), tuple("Jake", 20));
     }
 }
