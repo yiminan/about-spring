344b356755858043578f47a8554c74b8f3f539ea
orgojy <giveawesome@gmail.com>
2021-12-21 20:37:27 +0900

    Update AsyncController to process router for async test

--- src/main/java/com/example/aboutspring/async/AsyncController.java
+++ src/main/java/com/example/aboutspring/async/AsyncController.java
@@ -1,5 +1,28 @@
 package com.example.aboutspring.async;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class AsyncController {
+
+    private final AsyncService asyncService;
+
+    public AsyncController(AsyncService asyncService) {
+        this.asyncService = asyncService;
+    }
+
+    /**
+     * API Call!
+     * API End!
+     * Start!
+     * ...
+     * End!
+     */
+    @GetMapping("/test/async")
+    public void asyncTest() {
+        System.out.println("API Call!");
+        asyncService.asyncTest();
+        System.out.println("API End!");
+    }
 }
