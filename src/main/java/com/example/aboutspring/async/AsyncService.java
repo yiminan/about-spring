7dd618e6f1c0218abd8465d359f4623215a3430f
orgojy <giveawesome@gmail.com>
2021-12-21 20:33:20 +0900

    Add service component for async

---
+++ src/main/java/com/example/aboutspring/async/AsyncService.java
@@ -0,0 +1,8 @@
+package com.example.aboutspring.async;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AsyncService {
+}
